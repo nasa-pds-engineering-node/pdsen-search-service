4e38e2bb515ed291103bcc0b771a1c5dc1d75a3f
U-JPL\karpenko <karpenko@RAYL-AI846797.jpl.nasa.gov>
2019-09-30 15:15:48 -0700

    Basic spring-boot application

---
+++ search-server/src/main/java/gov/nasa/pds/search/SBMain.java
@@ -0,0 +1,13 @@
+package gov.nasa.pds.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBMain 
+{
+	public static void main(String[] args) 
+	{
+        SpringApplication.run(SBMain.class, args);
+    }
+}
---
+++ search-server/src/main/java/gov/nasa/pds/search/controller/UIController.java
@@ -0,0 +1,17 @@
+package gov.nasa.pds.search.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(path = "/ui")
+public class UIController 
+{
+	@GetMapping(path="/search", produces = "application/json")
+    public String getEmployees()
+    {
+        return "{}";
+    }
+}
